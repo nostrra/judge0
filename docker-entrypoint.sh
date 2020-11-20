b101e13542a9f1b8b8c5c84616be2042a4034756
nostrra <antonvoitko1@gmail.com>
2020-11-20 12:48:00 +0300

    Исправил пробелмы сборки, теперь сервер с проектом успешно разворачивается

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sudo touch /api/log/development.log
 sudo chmod 0664 /api/log/development.log
+sudo chmod -R 777 /api/scripts
+sudo chmod -R gu+rw /api/scripts
 sudo chmod -R 777 /api/log
 sudo chmod -R gu+rw /api/log
 sudo chmod -R 777 /var/run
@@ -9,4 +11,5 @@ sudo chmod -R 777 /api/tmp
 sudo chmod -R gu+rw /api/tmp
 sudo chmod -R gu+s /usr/sbin/cron
 cron
+bash /api/scripts/server
 exec "$@"
